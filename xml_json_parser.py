af38c10d11204b7fd455527e2b609a1d9ad0dc73
AMiT <theamit26@gmail.com>
2018-01-19 19:53:12 +0530

    Update xml_json_parser.py

--- xml_json_parser.py
+++ xml_json_parser.py
@@ -3,6 +3,7 @@
 # Make sure you have all the modules installed.
 # Author :: Amit Yadav
 
+
 import json
 import xmltodict
 
@@ -16,17 +17,29 @@ def xml_json_parse(string):
 
     try:
         simple_dict = json.loads(string)
+        # Need to change to dict if its just one value. ( maybe string or integer )
+        if type(simple_dict) != dict:
+            value = simple_dict
+            simple_dict = dict()
+            simple_dict[value] = value
+
 
     except:
         # comes here if the string is not a valid json string.
         try:
             simple_dict = xmltodict.parse(string)
-
+            # Need to change to dict if its just one value. ( maybe string or integer )
+            
+            if type(simple_dict) != dict:
+                value = simple_dict
+                simple_dict = dict()
+                simple_dict[value] = value
         except:
             # if neither json or xml string.
             print('ERROR :: "Not a valid xml or json string." ')
 
     return simple_dict
+
 
 
 
